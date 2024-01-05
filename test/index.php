eb843eb7bb92876508c16dde361eb26e69be0bac
Jesús Martín <jesus@jesusma.es>
2024-01-05 13:54:37 +0100

    TEST

--- test/index.php
+++ test/index.php
@@ -1,19 +1,107 @@
+<?php include('../helper/logon.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>Test page - Chechu</title>
+  <?php 
+  include_once('../helper/head.php'); 
+  $contacts = new Contacts();
+  $nuevas = $contacts->getAssigCountNew($contacts->getUserBySessid($_GET['id']))[0][0];
+  if($nuevas > 0)
+    $nuevas = "<span class='round'>".$nuevas."</span>";
+  else
+    $nuevas = "";
+
+  $enCurso = $contacts->getAssigCount($contacts->getUserBySessid($_GET['id']))[0][0];
+  if($enCurso > 0)
+    $enCurso = "<span class='round'>".$enCurso."</span>";
+  else
+    $enCurso = "";
+  ?>
 </head>
 <body>
-  <button id="boton">Abrir</button>
-  <span id="salida"></span>
-  <div id="notify" class="dialog"></div>
-  <div class="container">
-    <div class="animation">Chechu</div>
+  <div id="menu">
+    <?php include_once '../helper/menu.php'; ?>
   </div>
-<?php phpinfo(); ?>
+  <div class="search-table">
+    <div id="contacts" class="contacts">
+      <h1>Test</h1>
+      <section class="subButtons">
+        <button id="new"><?php echo $nuevas; ?> Nuevas Cesiones</button>
+        <button id="find">Buscar</button>
+        <button id="ready"><?php echo $enCurso; ?> En curso</button>
+        <button id="finish">Hechas</button>
+        <button id="status" class="active">Estadística</button>
+      </section>
+    </div>
+    <section class="assig-cpy" id="newTitle">Nueva cesión</section>
+    <div id="contacts-items">
+        <form>
+            <div class="form-group">
+              <section>
+                <label for="origen">*Origen</label>
+                <label for="destino">*Destino</label>
+                <select name="" id="origen">
+                  <option value="MADRID">Madrid</option>
+                  <option value="VIGO">Vigo</option>
+                  <option value="BARCELONA">Barcelona</option>
+                  <option value="ZARAGOZA">Zaragoza</option>
+                  <option value="VALENCIA">Valencia</option>
+                  <option value="GRANADA">Granada</option>
+                  <option value="SEVILLA">Sevilla</option>
+                  <option value="PALMA">Palma</option>
+                </select>
+                <select name="" id="destino">
+                  <option value="MADRID">Madrid</option>
+                  <option value="VIGO">Vigo</option>
+                  <option value="BARCELONA">Barcelona</option>
+                  <option value="ZARAGOZA">Zaragoza</option>
+                  <option value="VALENCIA">Valencia</option>
+                  <option value="GRANADA">Granada</option>
+                  <option value="SEVILLA">Sevilla</option>
+                  <option value="PALMA">Palma</option>
+                </select>
+                <div id="pclient" class="arrow-right active" style="grid-column: 1 / span 2;margin: auto;top: 2px;    border: 2px solid var(--bg-font-color);border-radius: 4px;box-shadow: 0px 0px 3px var(--bg-font-color);"></div>
+                <div id="provider" style="display:none;"></div>
+              </section>
+              <section>
+                <label for="client">*Cliente</label>
+                <input type="text" name="cliente" id="client">
+                <div id="clientName" class="clientNameAssign"></div>
+              </section>
+              <section>
+                <label for="pedido">Pedido</label>
+                <input type="text" name="pedido" id="pedido"></input>
+              </section>
+              <section>
+                <label for="coment">Comentarios</label>
+                <textarea type="text" name="Comentario" id="coment"></textarea>
+              </section>
+              <section>
+                <label for="ref">*Referencia PR</label>
+                <input type="text" name="Referencia" id="ref">
+                <div id="descRef" class="clientNameAssign"></div>
+              </section>
+              <section>
+                <label for="units">*Cant</label>
+                <input type="text" name="Cantidad" id="units">
+              </section>
+              <section>
+                <label for="nfm">NFM</label>
+                <input type="checkbox" name="NFM" id="nfm">
+              </section>
+              <section>
+                <label for="frag"><img width="40px" height="40px" alt="frágil" src="../img/wine_bar_FILL0_wght400_GRAD0_opsz40.png" alt="Frágil"></label>
+                <input type="checkbox" name="Frágil" id="frag">
+              </section>
+              <section>
+                <label for="send">.</label>
+                <input type="submit" value="🔽" id="send">
+              </section>
+            </div>
+        </form>
+        <div id="cesiones"></div>
+    </div>
+  </div>
+  <?php include('./../helper/footer.php'); ?>
 </body>
-<script src="test.js"></script>
 </html>
