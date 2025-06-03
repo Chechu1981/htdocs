9bad01b2b157b2cca0438337df93d1eec32e4294
Chechu <jesus@jesusma.es>
2025-06-03 17:45:01 +0200

    Actualiza la versión del script 'cesionesADV.js' y modifica la referencia de la imagen 'placasExternas.png' para corregir la carga de recursos.

--- js/cesionesADV.js
+++ js/cesionesADV.js
@@ -737,5 +737,5 @@ $$('form')[0].addEventListener('keyup',(e)=>{
 })
 
 $('openAssignPicture').addEventListener('click',()=>{
-  modal(`<div style="text-align:center"><img src="./../img/placasExternas.png?1005" /></div>`,"Cesiones dsiponibles entre placas externas")
+  modal(`<div style="text-align:center"><img src="./../img/placasExternas.png?1007" /></div>`,"Cesiones dsiponibles entre placas externas")
 })
